fbdb8e128cad9497d2071d1d5bbe3d62bc77a727
Max Ott <max.ott@data61.csiro.au>
2022-10-17 10:46:09 +1100

    Fixing sample app

--- sdk_service/examples/hello_world/hello_world.py
+++ sdk_service/examples/hello_world/hello_world.py
@@ -1,26 +1,20 @@
 from typing import Dict
 #TypedDict
-from ivcap_service import Service, Parameter, Option, Type, register_service
+from ivcap_sdk_service import Service, Parameter, PythonWorkflow, Type, register_service
 import logging
 
 SERVICE = Service(
     name = "HelloWorld",
     description = "Simple service which does a few simple things",
-    providerID = "ivcap:provider:0000-0000-0000",
     parameters = [
         Parameter(name="msg", type=Type.STRING, description="Message to echo"),
         Parameter(name="times", type=Type.INT, default=2, description="Times to repeat"),
-        Parameter(
-            name='device',
-            type=Type.OPTION,
-            options=[Option(value='cpu'), Option(value='gpu')],
-            default="gpu",
-            description="Select which device to inference."),
-    ]
+    ],
+    workflow = PythonWorkflow(min_memory='2Gi')
 )
 
 def hello_world(args: Dict, logger: logging):
-    for _ in range(args.times):
-        logger.info(f"Hello {args.msg}")
+    for i in range(args.times):
+        logger.info(f"({i + 1}) Hello {args.msg}")
 
 register_service(SERVICE, hello_world)
