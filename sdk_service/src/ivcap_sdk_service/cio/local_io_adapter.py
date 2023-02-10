55a71fd7a221e50b83642b09f6d353959ff0e1ce
Max Ott <max.ott@data61.csiro.au>
2023-02-10 11:37:17 +1100

    Added license header to all files in src

--- sdk_service/src/ivcap_sdk_service/cio/local_io_adapter.py
+++ sdk_service/src/ivcap_sdk_service/cio/local_io_adapter.py
@@ -1,3 +1,8 @@
+#
+# Copyright (c) 2023 Commonwealth Scientific and Industrial Research Organisation (CSIRO). All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file. See the AUTHORS file for names of contributors.
+#
 """
 FileAdapter a thin wrapper around io.IOBase that sets a storage path with
 standard filesystem backend
