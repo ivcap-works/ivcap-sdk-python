a3d25199966b7f92da0d771d8443043602da78c8
Max Ott <max.ott@data61.csiro.au>
2023-07-06 13:54:25 +1000

    service: Cleaned up examples

--- sdk_service/examples/text_on_image/img_test_service.py
+++ sdk_service/examples/text_on_image/img_test_service.py
@@ -1,3 +1,8 @@
+#
+# Copyright (c) 2023 Commonwealth Scientific and Industrial Research Organisation (CSIRO). All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file. See the AUTHORS file for names of contributors.
+#
 import sys, os
 SCRIPT_DIR=os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(SCRIPT_DIR, '../../src'))
